700a1c592eb7346a21a4d16a7ac2cfe947bed698
kenan aljaber <kenan.aljaber.p@gmail.com>
2022-11-22 16:27:50 -0500

    fixing the useState issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import DashBoard from './Components/DashBoard';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header headerText="Indice De Masa Corporal"/>
+      <DashBoard/>
     </div>
   );
 }
